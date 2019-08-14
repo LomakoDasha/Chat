f067fdad006fcfd4c447a12baf281385f085511b
Dasha <lomakodasha@gmail.com>
2019-08-14 17:12:51 +0300

    feat: add log out button to change user

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -3,11 +3,17 @@ import React, { Component } from 'react';
 import headerStyles from './header.module.css'
 
 class Header extends Component {
+    logOut = (e) => {
+        e.preventDefault();
+        localStorage.removeItem("username")
+        window.location = 'Chat/src/components/login/Login.js';
+    } 
+
   render () {
     return (
       <header className={headerStyles.header}>
           <h1 className={headerStyles.header__title}>Chat</h1>
-          <button className={headerStyles.header__button}>Log out</button>
+          <button className={headerStyles.header__button} onClick={this.logOut}>Log out</button>
       </header>
     )
   }
